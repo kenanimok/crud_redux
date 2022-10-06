af9645d5c9f44213bfccec60db830b87645d8998
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-10-06 15:09:54 +0700

    fix ui

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,34 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Body, Container, InputCustom, Label} from './login.style'
-const Login = ()=> {
+import React from "react";
+import styled from "styled-components";
+import { Body, Container, InputCustom, Label } from "./login.style";
+const Login = () => {
   return (
     <Body>
-        <Container>
+      <Container>
+        <div className="left">
+          <h1 className="welcome">Welcome</h1>
+        </div>
+        <div className="right">
+          <h1 className="login">LogIn</h1>
+          <InputCustom type="email"></InputCustom>
+          <Label>email</Label>
+          <InputCustom type="password"></InputCustom>
+          <Label>password</Label>
 
-             <div className='left'>
-             <h1 className="welcome">Welcome</h1>
-
-             </div>
-
-
-             <div className='right'>
-             <h1 className="login">LogIn</h1>
-               {/* <Label>ss</Label>
-               <InputCustom type={'email'}/> */}
-             {/* <input type="email" id="email" class="client-info" ></input> */}
-             <InputCustom type="email"></InputCustom>
-             <Label>email</Label>
-
-             <InputCustom type="password"></InputCustom>
-             <Label>password</Label>
-             <InputCustom type="submit"  style={{background: " #df98fa",color:"white",border:"none"}}></InputCustom>
-
-             </div>
-
-        </Container>
+          <InputCustom
+            type="submit"
+            style={{ background: " #df98fa", color: "white", border: "none" }}
+          ></InputCustom>
+        </div>
+      </Container>
     </Body>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
