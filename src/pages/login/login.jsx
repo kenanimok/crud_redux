433a38a27fdc330f266170069151bcee4e04019b
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-10-06 17:47:07 +0700

    fix connect

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-import { Body, Container, InputCustom, Label } from "./login.style";
+import {
+  Body,
+  ButtonResgis,
+  Container,
+  InputCustom,
+  Label,
+} from "./login.style";
+import { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+
 const Login = () => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const jsondata = {
+      username: username,
+      password: password,
+    };
+    axios
+      .post(`https://www.melivecode.com/api/login`, jsondata)
+      .then((res) => console.log(res));
+
+    // console.log("aaa", JSON.stringify(jsondata));
+  };
+
   return (
     <Body>
       <Container>
@@ -10,15 +37,49 @@ const Login = () => {
         </div>
         <div className="right">
           <h1 className="login">LogIn</h1>
-          <InputCustom type="email"></InputCustom>
-          <Label>email</Label>
-          <InputCustom type="password"></InputCustom>
-          <Label>password</Label>
 
-          <InputCustom
-            type="submit"
-            style={{ background: " #df98fa", color: "white", border: "none" }}
-          ></InputCustom>
+          <form onSubmit={handleSubmit}>
+            <InputCustom
+              type="text"
+              name="username"
+              onChange={(e) => setUserName(e.target.value)}
+            ></InputCustom>
+            <Label>email</Label>
+
+            <InputCustom
+              type="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></InputCustom>
+            <Label>password</Label>
+
+            <InputCustom
+              type="submit"
+              style={{ background: " #df98fa", color: "white", border: "none" }}
+            ></InputCustom>
+            <ButtonResgis>kkk</ButtonResgis>
+          </form>
+
+          {/* <label>
+              Enter your name:
+              <input
+                type="text"
+                id="username"
+                name="username"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </label>
+            <label>
+              Enter your age:
+              <input
+                type="text"
+                id="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <input type="submit" />
+          </form> */}
         </div>
       </Container>
     </Body>
--- src/pages/login/login.style.jsx
+++ src/pages/login/login.style.jsx
@@ -1,10 +1,8 @@
 import styled, { createGlobalStyle } from "styled-components";
-export  const Body = styled.div`
-
-    background-image: linear-gradient(310deg, #df98fa, #9055ff);
-    font-family: sans-serif;
-    height: 100vh;
-
+export const Body = styled.div`
+  background-image: linear-gradient(310deg, #df98fa, #9055ff);
+  font-family: sans-serif;
+  height: 100vh;
 `;
 export const Container = styled.div`
   position: absolute;
@@ -20,70 +18,62 @@ export const Container = styled.div`
   box-shadow: var(--box-shadow);
   transition-duration: 1s;
 
-
-
   .welcome {
-  font-size: 75px;
-  font-weight: 300;
-  margin-top: 330px;
-  text-shadow: var(--box-shadow);
-}
+    font-size: 75px;
+    font-weight: 300;
+    margin-top: 330px;
+    text-shadow: var(--box-shadow);
+  }
 
-.left {
-  background-image: url("https://images.unsplash.com/photo-1615400014497-55726234cccb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNzg4Njg0Ng&ixlib=rb-1.2.1&q=85");
-  background-size: cover;
-  background-position: center;
-  /* box-shadow: var(--box-shadow); */
- margin: auto;
-  width: 95%;
-  height: 96%;
-  border-radius: 20px; 
-}
-
-.login {
-  padding-top: 35%;
-  text-align: center;
-  text-transform: uppercase;
-  font-weight: 300;
-  font-size: 40px;
-  text-shadow: var(--box-shadow);
-}
-
-
-@media (max-width: 1250px) {
- 
-   width: 600px;
-   display: block;
-
-    .left {
-        display: none;
-    }
-
-    .right {
-    margin-top: 16px;
+  .left {
     background-image: url("https://images.unsplash.com/photo-1615400014497-55726234cccb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNzg4Njg0Ng&ixlib=rb-1.2.1&q=85");
     background-size: cover;
     background-position: center;
-    box-shadow: var(--box-shadow);
-    }
- 
-}
-
-
-@media (max-height: 850px) { 
-   width: 1000px;
-   height: 600px;
-   /* display: block; */
-   .welcome {
-    margin-top: 240px;
-    font-size: 60px;
+    /* box-shadow: var(--box-shadow); */
+    margin: auto;
+    width: 95%;
+    height: 96%;
+    border-radius: 20px;
   }
-}
 
+  .login {
+    padding-top: 35%;
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 300;
+    font-size: 40px;
+    text-shadow: var(--box-shadow);
+  }
 
-`
+  @media (max-width: 1250px) {
+    width: 600px;
+    display: block;
+
+    .left {
+      display: none;
+    }
+
+    .right {
+      margin-top: 16px;
+      background-image: url("https://images.unsplash.com/photo-1615400014497-55726234cccb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNzg4Njg0Ng&ixlib=rb-1.2.1&q=85");
+      background-size: cover;
+      background-position: center;
+      box-shadow: var(--box-shadow);
+    }
+  }
+
+  @media (max-height: 850px) {
+    width: 1000px;
+    height: 600px;
+    /* display: block; */
+    .welcome {
+      margin-top: 240px;
+      font-size: 60px;
+    }
+  }
+`;
 export const InputCustom = styled.input`
- display: block;
+  display: block;
   margin: 20px auto;
   width: 60%;
   height: 50px;
@@ -96,8 +86,8 @@ export const InputCustom = styled.input`
 
   :focus {
     width: 70%;
-    }
-`
+  }
+`;
 export const Label = styled.label`
   position: absolute;
   margin: -76px 130px;
@@ -108,10 +98,21 @@ export const Label = styled.label`
   color: #999;
   /* transition: all 200ms; */
   text-shadow: var(--box-shadow);
+`;
+export const ButtonResgis = styled.button`
+  display: block;
+  margin: 20px auto;
+  width: 60%;
+  height: 50px;
+  border: solid #999 1px;
+  border-radius: 5px;
+  text-indent: 15px;
+  transition: all 200ms;
+  box-shadow: var(--box-shadow);
+  font-size: 18px;
+  background-image: linear-gradient(310deg, #df98fa, #9055ff);
 
-
-`
-
-
-
-  
+  :focus {
+    width: 70%;
+  }
+`;
